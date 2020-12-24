f303e6bca837055bb95c08473850b04305d07a7f
sebbyu <you.sebastian.1.618@gmail.com>
2020-12-23 20:11:53 -0500

    Added update function in user serializer

--- user/serializers.py
+++ user/serializers.py
@@ -22,6 +22,24 @@ class UserSerializer(serializers.ModelSerializer):
     user.save()
     return user
 
+  def update(self, instance, validated_data):
+    password = validated_data.pop('password')
+    instance.email = validated_data.get('email', instance.email)
+    instance.nickname = validated_data.get('nickname', instance.nickname)
+    instance.gender = validated_data.get('gender', instance.gender)
+    instance.ethnicity = validated_data.get('ethnicity', instance.ethnicity)
+    instance.age = validated_data.get('age', instance.age)
+    instance.location = validated_data.get('location', instance.location)
+    instance.bio = validated_data.get('bio', instance.bio)
+    instance.set_password(password)
+    instance.save()
+    return instance
+
+  # instance.email = validated_data.get('email', instance.email)
+  #       instance.content = validated_data.get('content', instance.content)
+  #       instance.created = validated_data.get('created', instance.created)
+
+
   # def to_representation(self, instance):
   #   response = super().to_representation(instance)
   #   response['location'] = LocationSerializer(instance.location).data
