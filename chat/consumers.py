ba5522c3baf6c347e9e0ff15485f9537b171fe4d
sebbyu <you.sebastian.1.618@gmail.com>
2021-01-05 20:07:25 -0500

    proceeded to Chat Consumer methods

--- chat/consumers.py
+++ chat/consumers.py
@@ -3,6 +3,7 @@ from channels.generic.websocket import WebsocketConsumer
 class ChatConsumer(WebsocketConsumer):
   def connect(self):
     self.accept()
+    self.send(text_data="[Welcome!]")
   def disconnect(self, close_code):
     pass
   def receive(self, text_data):
