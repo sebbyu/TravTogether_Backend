3d404222676f95fd6978ae6ce40b3300f43f50ad
sebbyu <you.sebastian.1.618@gmail.com>
2021-01-06 18:16:43 -0500

    Redis Added for Synchronous Chatting

--- backend/settings.py
+++ backend/settings.py
@@ -163,3 +163,13 @@ EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = os.getenv('EMAIL_PORT')
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [('127.0.0.1', 6379)],
+        }
+    }
+}
--- chat/consumers.py
+++ chat/consumers.py
@@ -1,13 +1,40 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
+from django.utils import timezone
 class ChatConsumer(WebsocketConsumer):
   def connect(self):
+    self.user = self.scope['user']
+    # print(self.scope)
+    self.id = self.scope['url_route']['kwargs']['chat_id']
+    self.room_group_name = 'chat_%s' % self.id
+    async_to_sync(self.channel_layer.group_add)(
+      self.room_group_name,
+      self.channel_name
+    )
     self.accept()
+
   def disconnect(self, close_code):
-    pass
+    async_to_sync(self.channel_layer.group_discard)(
+      self.room_group_name,
+      self.channel_name
+    )
   def receive(self, text_data):
     text_data_json = json.loads(text_data)
     userNickname, newText = text_data_json['userNickname'], \
     text_data_json['newText']
-    self.send(text_data=json.dumps({'userNickname': userNickname,
-    'newText': newText}))
+    now = timezone.now()
+    async_to_sync(self.channel_layer.group_send)(
+      self.room_group_name,
+      {
+        'type': 'chat_message',
+        'message': newText,
+        'user': self.user.username,
+        'userNickname': userNickname,
+        'created': now.isoformat(),
+      }
+    )
+    # self.send(text_data=json.dumps({'userNickname': userNickname,
+    # 'newText': newText}))
+  def chat_message(self, event):
+    self.send(text_data=json.dumps(event))
