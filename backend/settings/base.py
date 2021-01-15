e3b03b6258fb450d0cf671341dcf580a412899ed
sebbyu <you.sebastian.1.618@gmail.com>
2021-01-14 23:51:28 -0500

    fixed base

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -155,9 +155,6 @@ EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = os.getenv('EMAIL_PORT')
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = 'ystephen0522@gmail.com'
-EMAIL_HOST_PASSWORD = 
 
 
 CHANNEL_LAYERS = {
