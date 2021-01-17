13f5dafd19a6c0cc65d8512245c7a4e2a4c49493
sebbyu <you.sebastian.1.618@gmail.com>
2021-01-16 22:00:34 -0500

    production testing

--- backend/asgi.py
+++ backend/asgi.py
@@ -21,7 +21,8 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 import os
 import django
 from channels.routing import get_default_application
+from backend.current_settings import CURRENT_SETTING
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", CURRENT_SETTING)
 django.setup()
 application = get_default_application
