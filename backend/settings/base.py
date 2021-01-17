11a5533cff866b241c9e02c40a304d2edb5ad9fc
sebbyu <you.sebastian.1.618@gmail.com>
2021-01-16 21:59:49 -0500

    production testing

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -168,7 +168,7 @@ CACHES = {
         "BACKEND": "django_redis.cache.RedisCache",
         "LOCATION": [os.environ.get("REDIS_URL")],
         "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"
         }
 
     }
